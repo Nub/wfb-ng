237817a6f8a3ee7b512d8a65bee555768c0b0ef0
Zachry Thayer <zachthayer@gmail.com>
2024-05-30 18:50:44 +0000

    Add systemd module

--- flake.nix
+++ flake.nix
@@ -11,22 +11,6 @@
         # Select host system packages
         pkgs = nixpkgs.legacyPackages.${system};
 
-        wfb_cfg = pkgs.writeText "wifibroadcast.cfg" ''
-        [common]
-        wifi_channel = 161     # 161 -- radio channel @5825 MHz, range: 5815–5835 MHz, width 20MHz
-                               # 1 -- radio channel @2412 Mhz, 
-                               # see https://en.wikipedia.org/wiki/List_of_WLAN_channels for reference
-        wifi_region = 'BO'     # Your country for CRDA (use BO or GY if you want max tx power)  
-
-        [gs_mavlink]
-        peer = 'connect://127.0.0.1:14550'  # outgoing connection
-        # peer = 'listen://0.0.0.0:14550'   # incoming connection
-
-        [gs_video]
-        peer = 'connect://127.0.0.1:5600'  # outgoing connection for
-                                           # video sink (QGroundControl on GS)
-        '';
-
         wfb-cli = pkgs.python3Packages.buildPythonApplication rec {
           name = "wfb_cli";
           src = ./.;
@@ -41,6 +25,7 @@
             pyserial
             setuptools
             wfb-ng
+            pkgs.iw
           ];
           nativeBuildInputs = propagatedBuildInputs;
           buildInputs = propagatedBuildInputs;
@@ -80,23 +65,20 @@
           cp wfb_rx $out/bin
           cp wfb_tx $out/bin
           cp wfb_keygen $out/bin
-
-          mkdir -p $out/etc
-          cp ${wfb_cfg} $out/etc/wifibroadcast.cfg
           '';
         };
 
 
       in rec {
         # Systemd unit
-        # nixosModules.themis = import ./module.nix packages.default;
+        nixosModules.wfb = (import ./module.nix) packages.default;
 
         # For `nix build` & `nix run`:
         packages.default = wfb-cli;
 
         # For `nix develop`:
         devShell = pkgs.mkShell {
-          nativeBuildInputs = [wfb-cli wfb-ng pkgs.iw];
+          nativeBuildInputs = [wfb-cli wfb-ng];
         };
       });
 }
---
+++ module.nix
@@ -0,0 +1,73 @@
+wfb_pkg:
+{ lib, config, ... }:
+with lib;
+let cfg = config.services.wfb;
+in {
+  options.services.wfb = {
+    enable = mkEnableOption "Enable WFB-NG Module";
+    pkg = mkOption {
+      type = types.pkg;
+      default = wfb_pkg;
+      description = "The wfb package to use";
+    };
+    profiles = mkOption {
+      type = types.list;
+      default = [ "udp_gs" ];
+      apply = x: strings.concatStrings (strings.intersperse ":" x);
+      description = "The profiles to run from the config";
+    };
+    interfaces = mkOption {
+      type = types.list;
+      default = [ "wfb0" ];
+      apply = x: strings.concatStrings (strings.intersperse " " x);
+      description = "The interface to run the server on";
+    };
+    # TODO: support config files for providing profiles etc
+    # config = mkOption {
+    #   type = types.pkg;
+    #   default = default_cfg
+    # };
+    # TODO:
+    # tx_power = mkOption {
+    #   type = types.int;
+    #   default = 20;
+    #   description = "The TX power to configure the interface to use";
+    # };
+  };
+
+  config = mkIf cfg.enable {
+    boot.extraModulePackages = with config.boot.kernelPackages;
+      [ rtl88xxau-aircrack ];
+
+    networking.firewall.interfaces.wfb0 = {
+      allowedTCPPorts = [ 22 2222 14550 9000 9001 ];
+      allowedUDPPortRanges = [{
+        from = 14000;
+        to = 15000;
+      }];
+    };
+
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="net", DRIVERS=="rtl88XXau", NAME="wfb0"
+    '';
+
+    systemd.services.wfb = {
+      enable = cfg.enable;
+      description = "WFB-NG service";
+
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      restartIfChanged = true;
+
+      serviceConfig = {
+        User = "root";
+        WorkingDirectory = "${cfg.working_dir}";
+        ExecStart =
+          "${cfg.pkg}/bin/wfb-server ${cfg.profiles} ${cfg.interfaces}";
+        Restart = "always";
+      };
+    };
+
+  };
+}
